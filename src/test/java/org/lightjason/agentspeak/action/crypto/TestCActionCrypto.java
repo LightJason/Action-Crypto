2c35d31935a77b4ccb8e06cf6a54ff095635f1f6
Philipp Kraus <philipp.kraus@flashpixx.de>
2021-02-06 14:53:55 +0100

    fixing junit5 test

--- src/test/java/org/lightjason/agentspeak/action/crypto/TestCActionCrypto.java
+++ src/test/java/org/lightjason/agentspeak/action/crypto/TestCActionCrypto.java
@@ -23,17 +23,12 @@
 
 package org.lightjason.agentspeak.action.crypto;
 
-import com.codepoetics.protonpack.StreamUtils;
-import com.tngtech.java.junit.dataprovider.DataProvider;
-import com.tngtech.java.junit.dataprovider.DataProviderRunner;
-import com.tngtech.java.junit.dataprovider.UseDataProvider;
-import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.apache.commons.lang3.tuple.ImmutableTriple;
 import org.apache.commons.lang3.tuple.Pair;
-import org.apache.commons.lang3.tuple.Triple;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.lightjason.agentspeak.error.context.CExecutionException;
 import org.lightjason.agentspeak.error.context.CExecutionIllegalStateException;
 import org.lightjason.agentspeak.error.context.CExecutionIllegealArgumentException;
@@ -55,7 +50,6 @@ import java.util.stream.Stream;
 /**
  * test action crypto
  */
-@RunWith( DataProviderRunner.class )
 public final class TestCActionCrypto extends IBaseTest
 {
 
@@ -64,129 +58,15 @@ public final class TestCActionCrypto extends IBaseTest
      *
      * @return data
      */
-    @DataProvider
-    public static Object[] generatehash()
+    public static Stream<Arguments> generatehash()
     {
-        return StreamUtils.zip(
-                    Stream.of( "adler-32", "crc-32", "crc-32c", "murmur3-32", "murmur3-128", "siphash-2-4" ),
-                    Stream.of( new String[]{"7804c01a", "911c63b0"}, new String[]{"45154713", "29369833"},
-                               new String[]{"387e0716", "4411bf68"}, new String[]{"306202a8", "08b9852d"},
-                               new String[]{"636cc4ff5f7ed59b51f29d6d949b4709", "f4459439308d1248efc0532fb4cd6d79"},
-                               new String[]{"4f27c08e5981bc5a", "82ee572bf0a0dde4"}
-                    ),
-
-                    ImmutablePair::new
-        ).toArray();
-    }
-
-    /**
-     * test crypt key generation
-     *
-     * @param p_crypt crypt definition
-     */
-    @Test
-    @UseDataProvider( "generatecrypt" )
-    public void createkey( final Triple<String, Integer, Integer> p_crypt )
-    {
-        final List<ITerm> l_return = new ArrayList<>();
-
-        new CCreateKey().execute(
-            false, IContext.EMPTYPLAN,
-            Stream.of( CRawTerm.of( p_crypt.getLeft() ) ).collect( Collectors.toList() ),
-            l_return
-        );
-
-        Assert.assertEquals( p_crypt.getMiddle().intValue(), l_return.size() );
-    }
-
-
-    /**
-     * test wrong algorithm
-     *
-     * @throws NoSuchAlgorithmException is thrown on key generator error
-     */
-    @Test( expected = CExecutionIllegealArgumentException.class )
-    public void wrongalgorithm() throws NoSuchAlgorithmException
-    {
-        final Key l_key = KeyGenerator.getInstance( "HmacSHA1" ).generateKey();
-
-        new CEncrypt().execute(
-            false,
-            IContext.EMPTYPLAN,
-            Stream.of( l_key ).map( CRawTerm::of ).collect( Collectors.toList() ),
-            Collections.emptyList()
-        );
-
-        new CDecrypt().execute(
-            false,
-            IContext.EMPTYPLAN,
-            Stream.of( l_key ).map( CRawTerm::of ).collect( Collectors.toList() ),
-            Collections.emptyList()
-        );
-    }
-
-    /**
-     * test decrypt execute array
-     *
-     * @throws NoSuchAlgorithmException is thrown on key generator error
-     */
-    @Test( expected = CExecutionIllegalStateException.class )
-    public void decryptexecutionerror() throws NoSuchAlgorithmException
-    {
-        final Pair<Key, Key> l_key = ECryptAlgorithm.RSA.generateKey();
-        final List<ITerm> l_return = new ArrayList<>();
-
-
-        new CEncrypt().execute(
-            false,
-            IContext.EMPTYPLAN,
-            Stream.of( l_key.getLeft(), "xxx" ).map( CRawTerm::of ).collect( Collectors.toList() ),
-            l_return
-        );
-
-        Assert.assertEquals( 1, l_return.size() );
-
-        new CDecrypt().execute(
-            false,
-            IContext.EMPTYPLAN,
-            Stream.of( l_key.getLeft(), l_return.get( 0 ).<String>raw() ).map( CRawTerm::of ).collect( Collectors.toList() ),
-            l_return
-        );
-    }
-
-    /**
-     * test hashing
-     *
-     * @param p_hash hash value
-     */
-    @Test
-    @UseDataProvider( "generatehash" )
-    public void hash( final Pair<String, String[]> p_hash )
-    {
-        final List<ITerm> l_return = new ArrayList<>();
-
-        Assert.assertTrue(
-            execute(
-                new CHash(),
-                false,
-                Stream.of( CRawTerm.of( p_hash.getLeft() ), CRawTerm.of( "test string" ), CRawTerm.of( 1234 ) ).collect( Collectors.toList() ),
-                l_return
-            )
-        );
-
-        Assert.assertArrayEquals( p_hash.getRight(), l_return.stream().map( ITerm::<String>raw ).toArray( String[]::new ) );
-    }
-
-    /**
-     * test hash exception
-     */
-    @Test( expected = CExecutionException.class )
-    public void hashexception()
-    {
-        new CHash().execute(
-            false, IContext.EMPTYPLAN,
-            Stream.of( CRawTerm.of( "xxx" ), CRawTerm.of( 1234 ) ).collect( Collectors.toList() ),
-            Collections.emptyList()
+        return Stream.of(
+            Arguments.of( "adler-32", new String[]{"7804c01a", "911c63b0"} ),
+            Arguments.of( "crc-32", new String[]{"45154713", "29369833"} ),
+            Arguments.of( "crc-32c", new String[]{"387e0716", "4411bf68"} ),
+            Arguments.of( "murmur3-32", new String[]{"306202a8", "08b9852d"} ),
+            Arguments.of( "murmur3-128", new String[]{"636cc4ff5f7ed59b51f29d6d949b4709", "f4459439308d1248efc0532fb4cd6d79"} ),
+            Arguments.of( "siphash-2-4", new String[]{"4f27c08e5981bc5a", "82ee572bf0a0dde4"} )
         );
     }
 
@@ -195,48 +75,170 @@ public final class TestCActionCrypto extends IBaseTest
      *
      * @return data
      */
-    @DataProvider
-    public static Object[] generatecrypt()
+    public static Stream<Arguments> generatecrypt()
     {
         return Stream.of(
-            new ImmutableTriple<>( "des", 1, 0 ),
-            new ImmutableTriple<>( "aes", 1, 0 ),
-            new ImmutableTriple<>( "rsa", 2, 1 )
-        ).toArray();
+            Arguments.of( "des", 1, 0 ),
+            Arguments.of( "aes", 1, 0 ),
+            Arguments.of( "rsa", 2, 1 )
+        );
     }
+
+    /**
+     * test crypt key generation
+     *
+     * @param p_input input definition
+     * @param p_result result
+     * @param p_ignore testing value
+     */
+    @ParameterizedTest
+    @MethodSource( "generatecrypt" )
+    public void createkey( final String p_input, final Integer p_result, final Integer p_ignore )
+    {
+        final List<ITerm> l_return = new ArrayList<>();
+
+        new CCreateKey().execute(
+            false, IContext.EMPTYPLAN,
+            Stream.of( CRawTerm.of( p_input ) ).collect( Collectors.toList() ),
+            l_return
+        );
+
+        Assertions.assertEquals( p_result.intValue(), l_return.size() );
+    }
+
+
+    /**
+     * test wrong algorithm
+     */
+    @Test
+    public void wrongalgorithm() throws NoSuchAlgorithmException
+    {
+        final Key l_key = KeyGenerator.getInstance( "HmacSHA1" ).generateKey();
+
+        Assertions.assertThrows( CExecutionIllegealArgumentException.class,
+                                 () -> new CEncrypt().execute(
+                                     false,
+                                     IContext.EMPTYPLAN,
+                                     Stream.of( l_key ).map( CRawTerm::of ).collect( Collectors.toList() ),
+                                     Collections.emptyList()
+                                 )
+        );
+
+        Assertions.assertThrows( CExecutionIllegealArgumentException.class,
+                                 () -> new CDecrypt().execute(
+                                     false,
+                                     IContext.EMPTYPLAN,
+                                    Stream.of( l_key ).map( CRawTerm::of ).collect( Collectors.toList() ),
+                                    Collections.emptyList()
+                            )
+        );
+    }
+
+    /**
+     * test decrypt execute array
+     */
+    @Test
+    public void decryptexecutionerror() throws NoSuchAlgorithmException
+    {
+        final Pair<Key, Key> l_key = ECryptAlgorithm.RSA.generateKey();
+        final List<ITerm> l_return = new ArrayList<>();
+
+        new CEncrypt().execute(
+            false,
+            IContext.EMPTYPLAN,
+            Stream.of( l_key.getLeft(), "xxx" ).map( CRawTerm::of ).collect( Collectors.toList() ),
+            l_return
+        );
+
+        Assertions.assertEquals( 1, l_return.size() );
+
+        Assertions.assertThrows( CExecutionIllegalStateException.class,
+                                 () -> new CDecrypt().execute(
+                                    false,
+                                    IContext.EMPTYPLAN,
+                                    Stream.of( l_key.getLeft(), l_return.get( 0 ).<String>raw() ).map( CRawTerm::of ).collect( Collectors.toList() ),
+                                    l_return
+                                )
+        );
+    }
+
+    /**
+     * test hashing
+     *
+     * @param p_input input data
+     * @param p_return result
+     */
+    @ParameterizedTest
+    @MethodSource( "generatehash" )
+    public void hash( final String p_input, final String[] p_return )
+    {
+        final List<ITerm> l_return = new ArrayList<>();
+
+        Assertions.assertTrue(
+            execute(
+                new CHash(),
+                false,
+                Stream.of( CRawTerm.of( p_input ), CRawTerm.of( "test string" ), CRawTerm.of( 1234 ) ).collect( Collectors.toList() ),
+                l_return
+            )
+        );
+
+        Assertions.assertArrayEquals( p_return, l_return.stream().map( ITerm::<String>raw ).toArray( String[]::new ) );
+    }
+
+    /**
+     * test hash exception
+     */
+    @Test
+    public void hashexception()
+    {
+        Assertions.assertThrows( CExecutionException.class,
+                                 () -> new CHash().execute(
+                                     false, IContext.EMPTYPLAN,
+                                     Stream.of( CRawTerm.of( "xxx" ), CRawTerm.of( 1234 ) ).collect( Collectors.toList() ),
+                                     Collections.emptyList()
+                                 )
+        );
+    }
+
+
 
     /**
      * test key generation on error call
      */
-    @Test( expected = CExecutionIllegealArgumentException.class )
+    @Test
     public void createkeyError()
     {
-        new CCreateKey().execute(
-            false,
-            IContext.EMPTYPLAN,
-            Stream.of( CRawTerm.of( "test" ) ).collect( Collectors.toList() ),
-            Collections.emptyList()
+        Assertions.assertThrows( CExecutionIllegealArgumentException.class,
+                                 () -> new CCreateKey().execute(
+                                false,
+                                    IContext.EMPTYPLAN,
+                                    Stream.of( CRawTerm.of( "test" ) ).collect( Collectors.toList() ),
+                                    Collections.emptyList()
+                                )
         );
     }
 
     /**
      * test encrypting and decrypting
      *
-     * @param p_crypt tripel with input data
+     * @param p_input input data
+     * @param p_result result data
+     * @param p_index index position
      */
-    @Test
-    @UseDataProvider( "generatecrypt" )
-    public void encryptdecreypt( final Triple<String, Integer, Integer> p_crypt  )
+    @ParameterizedTest
+    @MethodSource( "generatecrypt" )
+    public void encryptdecreypt( final String p_input, final Integer p_result, final Integer p_index )
     {
         final List<ITerm> l_returnkey = new ArrayList<>();
 
         new CCreateKey().execute(
             false, IContext.EMPTYPLAN,
-            Stream.of( CRawTerm.of( p_crypt.getLeft() ) ).collect( Collectors.toList() ),
+            Stream.of( CRawTerm.of( p_input ) ).collect( Collectors.toList() ),
             l_returnkey
         );
 
-        Assert.assertEquals( p_crypt.getMiddle().intValue(), l_returnkey.size() );
+        Assertions.assertEquals( p_result.intValue(), l_returnkey.size() );
 
 
         final List<ITerm> l_returnencrypt = new ArrayList<>();
@@ -252,14 +254,14 @@ public final class TestCActionCrypto extends IBaseTest
 
         new CDecrypt().execute(
             false, IContext.EMPTYPLAN,
-            Stream.concat( Stream.of( l_returnkey.get( p_crypt.getRight() ) ), l_returnencrypt.stream() ).collect( Collectors.toList() ),
+            Stream.concat( Stream.of( l_returnkey.get( p_index ) ), l_returnencrypt.stream() ).collect( Collectors.toList() ),
             l_return
         );
 
 
-        Assert.assertEquals( 2, l_return.size() );
-        Assert.assertEquals( "test string", l_return.get( 0 ).raw() );
-        Assert.assertEquals( 12345, l_return.get( 1 ).<Number>raw() );
+        Assertions.assertEquals( 2, l_return.size() );
+        Assertions.assertEquals( "test string", l_return.get( 0 ).raw() );
+        Assertions.assertEquals( 12345, l_return.get( 1 ).<Number>raw() );
     }
 
 }
